3daeb0bbca19958fcf5bec298336b426a5d04b6a
bhaveshonnet@gmail.com <bhaveshonnet@gmail.com>
2018-07-24 23:10:21 -0400

    comment update

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot - Jenkins Branch Build Merge !!!!!";
+        return "Greetings from Spring Boot - Jenkins Branch Build Merge !!!";
     }
 
 }
